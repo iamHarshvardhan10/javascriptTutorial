532b9e40d1a5b3c1c3a54e2c35a30d5ad2f270dd
Harshvardhan <shreeharshpb11@gmail.com>
2023-06-25 23:05:39 +0530

    New commit

--- 27_ForLoopArray.js
+++ 27_ForLoopArray.js
@@ -2,30 +2,25 @@
 
 // array with length properties
 
-let fruits = ['apple','mongo','orange','banana','watermelon']
+let fruits = ["apple", "mongo", "orange", "banana", "watermelon"];
 
 console.log(fruits);
-
 
 // to access the length of array
 
 console.log(fruits.length);
 
-
 // to access the last index of array
 
-console.log(fruits.length-1);
+console.log(fruits.length - 1);
 
+let fruits2 = [];
+for (let i = 0; i <= fruits.length; i++) {
+  console.log(i);
+  console.log(fruits[i]);
+  console.log(fruits[i].toUpperCase());
 
-
-let fruits2 = []
-for (let i=0; i<= fruits.length; i++){
-    console.log(i);
-    console.log(fruits[i])
-    console.log(fruits[i].toUpperCase());
-    
-    fruits2.push(fruits[i])
-
+  fruits2.push(fruits[i]);
 }
 
-console.log(fruits2)
+console.log(fruits2);
