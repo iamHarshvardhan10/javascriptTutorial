7087c8aba04897f767e5f53e4d96b7a33d22e864
Harshvardhan <shreeharshpb11@gmail.com>
2024-05-23 20:42:17 +0530

    while loop

--- 19_WhileLoop.js
+++ 19_WhileLoop.js
@@ -21,12 +21,66 @@
 //  2) In while loop assign the condition what we want 
 
 
-let total = 0;
-let i = 0;
+// let total = 0;
+// let i = 0;
 
-while(i <= 100){
-    total = total + i;
-    i++
+// while(i <= 100){
+//     total = total + i;
+//     i++
+// }
+
+// console.log(total)
+
+console.time('while')
+let i = 0
+console.log(i)
+i++
+console.log(i)
+i++
+console.log(i)
+i++
+console.log(i)
+i++
+console.log(i)
+i++
+console.log(i)
+i++
+console.log(i)
+i++
+console.log(i)
+i++
+console.log(i)
+console.timeEnd('while')
+
+console.time('loop')
+let a = 0
+while (a <= 9) {
+    console.log(a)
+    a++
 }
+console.timeEnd('loop')
 
-console.log(total)
+
+
+// 
+
+// let b = 0
+// while (b >= 0) {
+//     if (b % 2 !== 0) {
+
+//         console.log(b)
+//     }
+//     b++
+// }
+
+
+// all natural number sum
+
+let number = 0
+let total = 0
+
+while(number <= 10){
+    total = total + number
+    number++
+    console.log(total)
+}
