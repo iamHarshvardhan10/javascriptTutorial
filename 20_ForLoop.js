7745b46da07d3f6da7692ed19ad69529c0e45780
Harshvardhan <shreeharshpb11@gmail.com>
2024-05-23 21:03:21 +0530

    for loop

--- 20_ForLoop.js
+++ 20_ForLoop.js
@@ -9,9 +9,33 @@
 // for example
 
 let total = 0;
-for (let i = 0; i <=10; i++){
+for (let i = 0; i <= 10; i++) {
     total = total + i;
     // console.log(total)
 }
 
 console.log(total)
+
+
+// first 10 number
+
+for (let i = 0; i < 10; i++) {
+    console.log(i)
+}
+
+// 
+
+for (i = 10; i >= 0; i--) {
+    console.log(i)
+}
+
+
+// reverse the string
+
+let str = "hello"
+let rev = ""
+for (let i = str.length - 1; i >= 0; i--) {
+    rev = rev + str[i]
+}
+console.log(rev)
+
